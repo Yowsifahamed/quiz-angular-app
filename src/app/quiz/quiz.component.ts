3427967d45b9cf288f303e685230748350678d4a
Yowsif Ahamed <yowsifahamed@gmail.com>
2022-08-07 00:59:14 +0530

    sounds added

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -11,6 +11,14 @@ export class QuizComponent implements OnInit, OnDestroy {
   quizRoleName: string = "";
   private sub: any;
   public getroleDetails: any;
+  quizEnabled: boolean = false;
+  selectedRigthAnswer: boolean = false;
+  selectedIndex: number = -1;
+  rightAnswerNumber: number = 10;
+  quizIndex: number = -1;
+  nextButtonEnabled: boolean = false;
+  maxSeconds: number = 14;
+  maxMicroSeconds: number = 1000;
   
   constructor(
     private route: ActivatedRoute,
@@ -20,6 +28,7 @@ export class QuizComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.getRouterParamRoleName();
     this.getReleventDataOfRole();
+    // this.playAudio();
   }
 
   getRouterParamRoleName(){
@@ -43,4 +52,59 @@ export class QuizComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
+
+  playAudio(){
+    let audio = new Audio();
+    audio.src = "../../../assets/audio/quiz_splash.mp3";
+    audio.load();
+    audio.play();
+  }
+
+  startQuiz(){
+    this.quizEnabled = true;
+    this.quizIndex++;
+    this.loadSeconds();
+    this.loadMicroSeconds();
+  }
+
+  selectedAnswer(index:any){
+    this.nextButtonEnabled = true;
+    this.selectedIndex = index;
+    this.rightAnswerNumber = this.getroleDetails.quiz_collection[this.quizIndex].correctAnswerIdx;
+    if(index == this.rightAnswerNumber){
+      this.selectedRigthAnswer = true;
+    }else{
+      this.selectedRigthAnswer = false;
+    }
+  }
+
+  nextQuiz(){
+    this.quizIndex++;
+    this.selectedIndex = -1;
+    this.nextButtonEnabled = false;
+  }
+
+  loadSeconds() {
+    var that = this;
+    let interval = setInterval(function () {
+      that.maxSeconds--;
+      if(that.maxSeconds < 1 ){
+        clearInterval(interval);
+    }
+      console.log("this.increaseCount",that.maxSeconds)
+    }, 1000);
+  }
+
+  loadMicroSeconds() {
+    var that = this;
+    let interval = setInterval(function () {
+      that.maxMicroSeconds--;
+      if(that.maxMicroSeconds < 1 ){
+        clearInterval(interval);
+    }
+      console.log("this.increaseCount",that.maxMicroSeconds)
+    }, 71);
+  }
+
+  
 }
