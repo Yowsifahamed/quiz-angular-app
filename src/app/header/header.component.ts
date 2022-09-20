1feddf6771590d94c036f2f49974c38332262346
Yowsif Ahamed <yowsifahamed@gmail.com>
2022-09-20 23:57:27 +0530

    getter setter added for selected nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 import { filter } from 'rxjs';
+import { DashboardService } from '../service/dashboard.service';
 
 @Component({
   selector: 'app-header',
@@ -9,18 +10,19 @@ import { filter } from 'rxjs';
 })
 export class HeaderComponent implements OnInit {
   headerArray = [
-    { nav : "🔥 Trending" },
-    { nav : "🌟 Latest" },
-    { nav : "🎬 Entertainment" },
-    { nav : "⚽ Sports" },
-    { nav : "✈️ Travel" },
-    { nav : "💡 Misc" }
+    { nav : "🔥 Trending", type: "Trending" },
+    { nav : "🎬 Entertainment", type: "entertainment" },
+    { nav : "⚽ Sports", type: "sports" },
+    { nav : "✈️ Travel", type: "travel" },
+    { nav : "💡 Misc", type: "misc" }
   ]
+
+  // { nav : "🌟 Latest", type: "Latest" },
 
   selectedArrayIndex: number = 0;
   currentRouterLink: string = "";
 
-  constructor(private router: Router) {
+  constructor(private router: Router, public dashboardService : DashboardService) {
     router.events.pipe(filter(e => e instanceof NavigationStart)).subscribe((e:any) => {
       this.currentRouterLink = e.url.split("/")[1];
     });
@@ -30,7 +32,8 @@ export class HeaderComponent implements OnInit {
   }
 
   navigationClickEvent(navigation:number){
-    this.selectedArrayIndex = navigation
+    this.selectedArrayIndex = navigation;
+    this.dashboardService.setSelectedNavigation(this.headerArray[navigation].type)
   }
 
 }
