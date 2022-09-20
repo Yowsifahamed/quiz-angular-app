66f50b7b6abf1d22060c8028557b97e0bc06f35c
Yowsif Ahamed <yowsifahamed@gmail.com>
2022-09-20 23:58:01 +0530

    behaviortal subject added

--- src/app/service/dashboard.service.ts
+++ src/app/service/dashboard.service.ts
@@ -1,14 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
+  private selectedNavigation = new BehaviorSubject<any>('Trending');
+  
   constructor(private httpClient: HttpClient) { }
 
   fetchDashboardData(){
     return this.httpClient.get('../../assets/db/quizCollection.json')
+  }
+
+  setSelectedNavigation(nav:string){
+    this.selectedNavigation.next(nav)
+  }
+
+  getSelectedNavigation(){
+    return this.selectedNavigation.asObservable();
   }
 }
